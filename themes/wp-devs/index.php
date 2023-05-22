4dae1bbe9fc1807561f381832e3fc3b8e179ce78
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-22 11:37:15 +0800

    segregate header, footer and content area

--- themes/wp-devs/index.php
+++ themes/wp-devs/index.php
@@ -1,41 +1,4 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-<head>
-    <meta charset="<?php bloginfo ('charset');  ?>">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Wordpress Dev</title>
-</head>
-<body <?php body_class(); ?>>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, odio officiis corrupti a porro, ratione nisi earum perferendis ipsa sunt nesciunt nulla eligendi. In a qui fugit eaque quam eum alias nesciunt quia ipsa dignissimos repudiandae deserunt, labore, esse voluptatum cumque culpa maiores beatae vitae iure ab similique ratione? Dolores?</p>
-    
-    <div class="site" id="page">
-        <header>
-
-        <section>
-            <div class="top-bar">
-                <div class="logo">
-
-                </div>
-
-                <div class="searchbox">
-                    
-            </div>            
-        </section>
-
-        <section class="menu-area">
-            <nav class="main-menu">
-                <ul>
-                    <li><a href="#">Home</a></li>
-                    <li><a href="#">About us</a></li>
-                    <li><a href="#">Services</a></li>
-                    <li><a href="#">Contact us</a></li>
-                </ul>
-            </nav>
-
-        </section>  
-
-        </header>
+<?php get_header(); ?>
 
         <!-- content area -->
         <div class="site-content" id="content">
@@ -50,22 +13,11 @@
                         Services
                     </section>
                     <sectiom class="home-blog">
-
+                        Blog 
                     </sectiom>
                 </div>
 
             </div>
 
         </div>
-
-        <!-- footer area -->
-
-        <footer class="site-footer">
-            Footer
-        </footer>
-
-
-    </div>
-    
-</body>
-</html>
+<?php get_footer(); ?>
