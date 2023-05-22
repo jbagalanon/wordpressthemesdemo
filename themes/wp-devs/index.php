3ec921219d641e618f96a7431b4bc4dd366b3187
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-23 02:58:21 +0800

    modify index.php to get the posts

--- themes/wp-devs/index.php
+++ themes/wp-devs/index.php
@@ -13,7 +13,30 @@
                         Services
                     </section>
                     <sectiom class="home-blog">
-                        Blog 
+                        <?php 
+                        if (have_posts()):
+                            while (have_posts(  )): the_post(  );
+                            ?> 
+                            <article> 
+                            <h2><?php the_title(); ?> </h2>
+                            <div class="meta-info">
+                            <p>Posted in <?php echo get_the_date(); ?>
+                            by <?php the_author_posts_link(); ?>                             
+                            </p>
+                            <p>Categories: <?php the_category('');  ?></p>
+                            <p>Tags: <?php the_tags('', ',  ' ); ?> </p>
+
+                            </div>
+                          
+
+                            <?php  the_content(); ?>
+                            </article>                         
+                            <?php
+                        endwhile;
+                        else: ?>                            ?>
+                            <p>There are no posts!</p> 
+                            <?php endif;                 
+                        ?>
                     </sectiom>
                 </div>
 
