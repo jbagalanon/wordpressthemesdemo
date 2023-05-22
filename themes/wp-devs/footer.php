d7ee82c46ea66f2fd80c4617f57773e19c55f10c
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-22 14:42:18 +0800

    added script and footer in footer.php

--- themes/wp-devs/footer.php
+++ themes/wp-devs/footer.php
@@ -7,6 +7,6 @@
 
 
     </div>
-    
+    <?php wp_footer(); ?>   
 </body>
 </html>
--- themes/wp-devs/functions.php
+++ themes/wp-devs/functions.php
@@ -8,6 +8,8 @@ function wpdevs_load_scripts (){
 
     wp_enqueue_style( 'google-fonts', 'https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap', array (), null );
 
+    wp_enqueue_script( 'dropdown', get_template_directory_uri(). '/js/dropdown.js', array(), '1.0.0', true );
+
     // wp_enqueue_style('wpdevs-main', get_template_directory_uri() . '/css/main.css', array(), '1.0.0', 'all');
     // wp_enqueue_style('wpdevs-responsive', get_template_directory_uri() . '/css/responsive.css', array(), '1.0.0', 'all');
     // wp_enqueue_style('wpdevs-fontawesome', get_template_directory_uri() . '/css/font-awesome.min.css', array(), '1.0.0', 'all');
