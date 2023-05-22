b9922a8f3db018d2760e04d86de0ab5173a7ddf2
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-22 10:49:15 +0800

    added wp-devs themes

---
+++ themes/wp-devs/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wordpress Dev</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem, odio officiis corrupti a porro, ratione nisi earum perferendis ipsa sunt nesciunt nulla eligendi. In a qui fugit eaque quam eum alias nesciunt quia ipsa dignissimos repudiandae deserunt, labore, esse voluptatum cumque culpa maiores beatae vitae iure ab similique ratione? Dolores?</p>
+    
+</body>
+</html>
