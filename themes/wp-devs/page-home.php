7a43afb4964c131b0b4c0ee2a50cd57815de9d77
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-23 04:06:59 +0800

    added page-home and page-about page

---
+++ themes/wp-devs/page-home.php
@@ -0,0 +1,58 @@
+<?php get_header(); ?>
+
+        <!-- content area -->
+        <div class="site-content" id="content">
+            <div class="content-area" id="primary">
+
+                <div class="site-main" id="main">
+                    <section class="hero">
+                        Hero
+
+                    </section>
+                    <section class="services">
+                        Services
+                    </section>
+                    <sectiom class="home-blog">
+
+                    <div class="container">
+
+                    <div class="blog-items">
+
+ <?php 
+                        if (have_posts()):
+                            while (have_posts(  )): the_post(  );
+                            ?> 
+                            <article> 
+                            <h2><?php the_title(); ?> </h2>
+                            <div class="meta-info">
+                            <p>Posted in <?php echo get_the_date(); ?>
+                            by <?php the_author_posts_link(); ?>                             
+                            </p>
+                            <p>Categories: <?php the_category('');  ?></p>
+                            <p>Tags: <?php the_tags('', ',  ' ); ?> </p>
+
+                            </div>
+                          
+
+                            <?php  the_content(); ?>
+                            </article>                         
+                            <?php
+                        endwhile;
+                        else: ?>                            ?>
+                            <p>There are no posts!</p> 
+                            <?php endif;                 
+                        ?>
+
+
+
+                    </div>
+
+                    </div>
+                       
+                    </sectiom>
+                </div>
+
+            </div>
+
+        </div>
+<?php get_footer(); ?>
