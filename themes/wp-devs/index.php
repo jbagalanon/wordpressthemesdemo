a6841777cb4fcd38af62c381afdcfd2e4b31d128
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-23 11:12:31 +0800

    added general template and add show current template plugins

--- themes/wp-devs/index.php
+++ themes/wp-devs/index.php
@@ -4,53 +4,45 @@
         <div class="site-content" id="content">
             <div class="content-area" id="primary">
 
-                <div class="site-main" id="main">
-                    <section class="hero">
-                        Hero
-
-                    </section>
-                    <section class="services">
-                        Services
-                    </section>
-                    <sectiom class="home-blog">
+                <div class="site-main" id="main">      
+                    <h1>Blog</h1>         
 
                     <div class="container">
 
-                    <div class="blog-items">
+                        <div class="blog-items">
 
- <?php 
-                        if (have_posts()):
-                            while (have_posts(  )): the_post(  );
-                            ?> 
-                            <article> 
-                            <h2><?php the_title(); ?> </h2>
-                            <div class="meta-info">
-                            <p>Posted in <?php echo get_the_date(); ?>
-                            by <?php the_author_posts_link(); ?>                             
-                            </p>
-                            <p>Categories: <?php the_category('');  ?></p>
-                            <p>Tags: <?php the_tags('', ',  ' ); ?> </p>
+                            <?php 
+                                if (have_posts()):
+                                        while (have_posts(  )): the_post(  );
+                                        ?> 
+                                        <article> 
+                                        <h2><?php the_title(); ?> </h2>
+                                        <div class="meta-info">
+                                        <p>Posted in <?php echo get_the_date(); ?>
+                                        by <?php the_author_posts_link(); ?>                             
+                                        </p>
+                                        <p>Categories: <?php the_category('');  ?></p>
+                                        <p>Tags: <?php the_tags('', ',  ' ); ?> </p>
 
-                            </div>
-                          
+                                        </div>                                   
 
-                            <?php  the_content(); ?>
-                            </article>                         
-                            <?php
-                        endwhile;
-                        else: ?>                            ?>
-                            <p>There are no posts!</p> 
-                            <?php endif;                 
-                        ?>
-
+                                        <?php  the_content(); ?>
+                                        </article>                         
+                                        <?php
+                                    endwhile;
+                                else: ?>                            
+                                    <p>There are no posts!</p> 
+                                    <?php endif;                 
+                            ?>
 
 
+
+                        </div>
+
+                        </div>
+                        
+                    
                     </div>
-
-                    </div>
-                       
-                    </sectiom>
-                </div>
 
             </div>
 
