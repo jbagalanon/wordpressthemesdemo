f2d38b72d28d6d29e638c31f1f11c3358e249d6b
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-24 01:16:25 +0800

    added custom logo function and add php coded in logo header

--- themes/wp-devs/header.php
+++ themes/wp-devs/header.php
@@ -1,46 +1,49 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
+
 <head>
-    <meta charset="<?php bloginfo ('charset');  ?>">
+    <meta charset="<?php bloginfo('charset');  ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head(); ?> 
+    <?php wp_head(); ?>
 </head>
+
 <body <?php body_class(); ?>>
-       
+
     <div class="site" id="page">
         <header>
 
-        <section>
-            <div class="top-bar">
-                <div class="logo">
-                Site Logo
-                </div>
-
-                <div class="searchbox">
-                    
-            </div>            
-        </section>
-
-        <section class="menu-area">
-            <div class="container">
-              <nav class="main-menu">
-                <button class="check-button">
-                    <div class="menu-icon">
-                        <div class="bar1"></div>
-                        <div class="bar2"></div>
-                        <div class="bar3"></div>
-
-
+            <section class="top-bar">
+                <div class="container">
+                    <div class="logo">
+                        <?php the_custom_logo(); ?>
                     </div>
 
+                    <div class="searchbox">
+                        Search
 
-                </button> 
-          <?php wp_nav_menu(array('theme_location' => 'wp_devs_main_menu', 'depth' => 2 )); ?>
-                </nav>
-            </div>
-        
+                    </div>
+            </section>
 
-        </section>  
+            <section class="menu-area">
+                <div class="container">
+                    <nav class="main-menu">
+                        <button class="check-button">
+                            <div class="menu-icon">
+                                <div class="bar1"></div>
+                                <div class="bar2"></div>
+                                <div class="bar3"></div>
+
+
+                            </div>
+
+
+                        </button>
+                        <?php wp_nav_menu(array('theme_location' => 'wp_devs_main_menu', 'depth' => 2)); ?>
+                    </nav>
+                </div>
+
+
+            </section>
 
         </header>
