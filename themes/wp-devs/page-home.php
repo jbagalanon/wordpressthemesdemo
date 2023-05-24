e1de8801a263ffdadc2a2008c2f806c9967d3b06
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-24 14:46:38 +0800

    add wp_query in page home and modify page behaviour

--- themes/wp-devs/page-home.php
+++ themes/wp-devs/page-home.php
@@ -8,7 +8,6 @@
         <div class="site-main" id="main">
             <section class="hero">
                 Hero
-
             </section>
             <section class="services">
                 <h2>Services</h2>
@@ -18,7 +17,6 @@
                             if (is_active_sidebar('services-1')){
                                 dynamic_sidebar('services-1');
                             }
-
                         ?>
                     </div>
                     <div class="services-item">
@@ -27,8 +25,6 @@
                              if (is_active_sidebar('services-2')){
                                 dynamic_sidebar('services-2');
                             }
-
-
                         ?>
                     </div>
                     <div class="services-item">
@@ -37,38 +33,50 @@
                              if (is_active_sidebar('services-3')){
                                 dynamic_sidebar('services-3');
                             }
-
-
                         ?>
                     </div>
                 </div>
             </section>
             <sectiom class="home-blog">
 
-                <div class="container">
-
-                    <div class="blog-items">
-
+                    <h2>Latest News</h2>
+                <div class="container">      
                         <?php
-                        if (have_posts()) :
-                            while (have_posts()) : the_post();
+                        $args = array(
+                            'post_type' => 'post',
+                            'post_per_page' => 5,
+                            'category__in' => array(3,6,9
+                            ),
+                            'category__not_in'=> array(1  
+                            )
+                        );
+
+                $postlist = new WP_Query($args);
+
+
+                        if ($postlist -> have_posts()) :
+                            while ($postlist -> have_posts()) : $postlist -> the_post();
                         ?>
-                                <article>
-                                    <h2><?php the_title(); ?> </h2>
-                                    <div class="meta-info">
-                                        <p>Posted in <?php echo get_the_date(); ?>
-                                            by <?php the_author_posts_link(); ?>
-                                        </p>
-                                        <p>Categories: <?php the_category('');  ?></p>
-                                        <p>Tags: <?php the_tags('', ',  '); ?> </p>
+                            <article class='latest-news'>
+                                
+                                <?php the_post_thumbnail('large');  ?>
+                                <h3><?php the_title(); ?> </h3>
+                                <div class="meta-info">
+                                    <p>
+                                    by <span> <?php the_author_posts_link(); ?> </span>
+                                    Categories: <span><?php the_category(' ');  ?> </span>
+                                    Tags: <?php the_tags('', ',  '); ?>
+                                    </p>
 
-                                    </div>
+                                    <p><span><?php echo get_the_date(); ?></span></p>
 
+                                </div>
 
-                                    <?php the_content(); ?>
-                                </article>
+                                <?php the_excerpt(); ?>
+                            </article>
                             <?php
                             endwhile;
+                            wp_reset_postdata();
                         else : ?> ?>
                             <p>There are no posts!</p>
                         <?php endif;
@@ -76,7 +84,7 @@
 
 
 
-                    </div>
+                    
 
                 </div>
 
