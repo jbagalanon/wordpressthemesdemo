b6fed307c0b051980e884ab366f84f5476821732
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-26 14:02:09 +0800

    cleaning the files by separating redundant info

--- themes/wp-devs/single.php
+++ themes/wp-devs/single.php
@@ -5,53 +5,26 @@
     <div class="container">
             <?php
                 while (have_posts()) : the_post();
-            ?>
+            ?>                        
 
-                        <article id="post-<?php the_ID();?> " <?php post_class()?> >
+        <?php get_template_part('parts/content', 'single'); ?>
 
-                            <header>
-                                    <h1><?php the_title(); ?></h1>                    
-                                    <div class="meta-info">
-                                        <p>Posted in <?php echo get_the_date(); ?>
-                                        by <?php the_author_posts_link(); ?>
-                                        </p>
-                                        <p>Categories: <?php the_category('');  ?></p>
-                                        <p>Tags: <?php the_tags('', ',  '); ?> </p>
-                                    </div>
-                            </header>
+        <div class="wpdevs-pagination">                                
+            <div class="pages next">
+                <?php next_post_link('&laquo; %link');?>
+            </div>
 
-                                    <?php the_content(); ?>
-                                    <?php wp_link_pages('nextpagelink')?>
-                        </article>
-
-                                <div class="wpdevs-pagination">                                
-                        
-                                <div class="pages next">
-                                     <?php next_post_link('&laquo; %link');?>
-                                             
-                            </div>
-
-                            <div class="pages previous">
-                               
-                                  <?php previous_post_link ('%link &raquo;'); ?>             
-                            </div>
-                            </div>
+        <div class="pages previous">
+            <?php previous_post_link ('%link &raquo;'); ?>             
+        </div>
+    </div>
                             
-
-
-
-                        <?php 
-                        if (comments_open() || get_comments_number()){
-                            comments_template();
-                        }
-                        
-                        
-
-            
-
+        <?php 
+            if (comments_open() || get_comments_number()){
+                comments_template();
+            }
         endwhile;
         ?>
-        
 </div>
 </div>
 
