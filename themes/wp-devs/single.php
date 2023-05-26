9a1d326963ca283bb7a9c5bbf8f3e3ebb4960150
Joemar Bagalanon <31525007+jbagalanon@users.noreply.github.com>
2023-05-26 10:10:27 +0800

    added next and previous post in single.php

--- themes/wp-devs/single.php
+++ themes/wp-devs/single.php
@@ -23,6 +23,22 @@
                                     <?php the_content(); ?>
                         </article>
 
+                                <div class="wpdevs-pagination">                                
+                        
+                                <div class="pages next">
+                                     <?php next_post_link('&laquo; %link');?>
+                                             
+                            </div>
+
+                            <div class="pages previous">
+                               
+                                  <?php previous_post_link ('%link &raquo;'); ?>             
+                            </div>
+                            </div>
+                            
+
+
+
                         <?php 
                         if (comments_open() || get_comments_number()){
                             comments_template();
